319af7c000f5f4483ab27c6e79dc273df60fc31e
Francisco Berti da Cruz <fbc991@live.com>
2023-07-04 01:00:09 -0300

    .

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,15 +1,43 @@
+import { useState } from 'react';
+
 const Tour = ({ id, name, info, image, price }) => {
+  const [flagInfo, setFlagInfo] = useState(false);
+
+  const infoReduce = (info) => {
+    if (info.length > 199) {
+      return `${info.slice(0, 199)} ... `;
+    }
+  };
+
   return (
     <article className="tour">
       <img src={image} alt={name} className="img img-tour" />
       <span className="span-tour ">${price}</span>
       <div className="div-tour">
         <h4>{name}</h4>
-        <p className="info">{info}</p>
-        {/* 203 with space + ... */}
-        <button type="button" className="btn btn-tour-read">
-          read more
-        </button>
+        {flagInfo ? (
+          <>
+            <p className="info">{info}</p>
+            <button
+              type="button"
+              className="btn btn-tour-read"
+              onClick={() => setFlagInfo(false)}
+            >
+              show less
+            </button>
+          </>
+        ) : (
+          <>
+            <p className="info">{infoReduce(info)}</p>
+            <button
+              type="button"
+              className="btn btn-tour-read"
+              onClick={() => setFlagInfo(true)}
+            >
+              read more
+            </button>
+          </>
+        )}
         <button type="button" className="btn  btn-block btn-tour">
           not interested
         </button>
